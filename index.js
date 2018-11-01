a89ef70e5ee8e170de6219b14574caa25a70d3a9
Mary Palumbo <mpalumbo@conductor.com>
2018-11-01 23:02:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,10 @@ function fillInMovieData() {
  const genreDiv = document.querySelector('#genre')
  genreDiv.innerText = genre
  
+ const genre = movies['Titanic'].genre
+ const genreDiv = document.querySelector('#genre')
+ genreDiv.innerText = genre
+ 
+ 
 }
 
