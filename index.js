5bd606767b2d6f8c163d808989aaa01217cbb9f9
Mary Palumbo <mpalumbo@conductor.com>
2018-11-01 23:33:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,40 +11,51 @@ document.addEventListener("DOMContentLoaded", (e) => {
 function fillInMovieData() {
   const currentMovie = movies['Titanic']
   
-  const title = currentMovie.title
+  const title = currentMovie.title // "Titanic"
   const titleDiv = document.querySelector('#title')
   titleDiv.innerText = title
-
+  
   const director = currentMovie.director
   const directorDiv = document.querySelector('#director')
   directorDiv.innerText = director
- 
+  
   const genre = currentMovie.genre
   const genreDiv = document.querySelector('#genre')
   genreDiv.innerText = genre
- 
+  
   const filmRating = currentMovie.filmRating
   const filmRatingDiv = document.querySelector('#filmRating')
   filmRatingDiv.innerText = filmRating
- 
+  
   const poster = currentMovie.poster
   const posterImg = document.querySelector('#poster')
   posterImg.src = poster
- 
+  
   const description = currentMovie.description
   const descriptionDiv = document.querySelector('#description')
   descriptionDiv.innerText = description
- 
+  
   const audienceScore = currentMovie.audienceScore
   const audienceScoreDiv = document.querySelector('#audienceScore')
   audienceScoreDiv.innerText = audienceScore
   
   for (let castMember of currentMovie.cast) {
+    console.log(castMember)
     const li = document.createElement('li')
     // <li></li>
     li.innerText = castMember.role + ': ' + castMember.actor
     const div = document.querySelector('#cast')
     div.appendChild(li)
+    
+  }
+  
+   for (let review of currentMovie.reviews) {
+    const li = document.createElement('li')
+    // <li></li>
+    li.innerText = review.username + ': ' + review.content
+    const div = document.querySelector('#reviews')
+    div.appendChild(li)
+}
     
   for (let review of currentMovie.reviews) {
     const li = document.createElement('li')
